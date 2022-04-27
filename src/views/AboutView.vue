1bbdf5d8e6a642b63f3e65dcabcd0c02a514c2fd
David2261 <bulatnasirov2003@gmail.com>
2022-04-27 17:02:12 +0300

    First push poor project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <BeakerIcon class="h-5 w-5 text-blue-500"/>
+    <p>...</p>
+  </div>
+</template>
+
+<script>
+import { BeakerIcon } from '@heroicons/vue/solid'
+
+export default {
+  components: { BeakerIcon }
+}
+</script>
